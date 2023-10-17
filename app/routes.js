5876d5c4583fe55567f005c4bd49f1dbcfdeecb4
melodycurtis <melodycurtis123@gmail.com>
2023-10-17 13:59:52 +0100

    hh

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,8 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+router.use((req, res, next) => {
+    res.locals.url = req.url
+    next()
+})
